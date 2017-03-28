1a2ee3daf3bf00d43c9091d2ae5daa991f72894a
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-03-28 02:42:32 +0100

    moved User and Todo into seperate models, and tested api with POSTMAN

--- server/server.js
+++ server/server.js
@@ -1,52 +1,30 @@
 /*jshint esversion: 6 */
-var mongoose  = require("mongoose");
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-    required:true,
-    minlength: 1,
-    trim: true
-  },
-  completed:{
-    type: Boolean,
-    default: false
-  },
-  completedAt:{
-      type: Number,
-      default: null
-  }
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req,res) => {
+    var todo = new Todo({
+      text : req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    },(e) => {
+        res.status(400).send(e);
+    });
 });
 
-// var otherTodo = new Todo({
-//   text:'   Edit my project proposal   '
-// });
-//
-// otherTodo.save().then((doc) => {
-//     console.log(JSON.stringify(doc,undefined,2));
-// }, (e) => {
-//   console.log('unable to save todo', e);
-// });
 
-
-var User = mongoose.model('User', {
-  email: {
-    type : String,
-    required: true,
-    trim: true,
-    minlength: 1
-  }
-});
-
-var user = new User({
-    email: 'ibukunbamise@yahoo.com'
-});
-
-user.save().then((doc) => {
-    console.log('User Saved', doc);
-}, (e) => {
-    console.log('unable to save user', e);
+app.listen(3000, () => {
+  console.log("Started on port 3000");
 });
